87c10ce0c7dae2358747fd54ab0f30f8801b5db4
G r e y <vapurrmaid@gmail.com>
2019-10-27 18:45:19 -0400

    :tada: Init UI (#7)

---
+++ src/WebApp/src/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Layout.css'
+
+export const Layout: React.FC = ({ children }) => (
+  <>
+
+    <header className="page-head">
+      <h1>Gotherator</h1>
+    </header>
+
+    <main className="page-content">
+      {children}
+    </main>
+
+    <footer className="page-foot">
+      <h2>Gotherator Footer</h2>
+    </footer>
+
+  </>
+);
