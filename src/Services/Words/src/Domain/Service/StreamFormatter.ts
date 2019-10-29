73ac385bbc11dfefd9b2d417503ca3a09829bcf2
G r e y <vapurrmaid@gmail.com>
2019-10-28 23:38:08 -0400

    :sparkles: Add Decorators (#14)

--- src/Services/Words/src/Domain/Service/StreamFormatter.ts
+++ src/Services/Words/src/Domain/Service/StreamFormatter.ts
@@ -1,7 +1,12 @@
+import { Decorator } from '../Model/Decorator';
 import { GrammarObject } from '../Model/GrammarObject';
 
 export class StreamFormatter {
   public constructor(private readonly stream: GrammarObject[]) { }
+
+  public toDecoratedString(): string {
+    return Decorator.decorateString(this.toUndecoratedString());
+  }
 
   public toUndecoratedString(): string {
     return this.stream
