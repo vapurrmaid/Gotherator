15335b425abb684365689eabf01709bdf3c4cddc
Vapurrmaid <vapurrmaid@gmail.com>
2019-10-29 08:14:22 -0400

    :sparkles: Add Determiners

--- src/Services/Words/src/Domain/Model/Determiner.ts
+++ src/Services/Words/src/Domain/Model/Determiner.ts
@@ -1,3 +1,39 @@
 import { GrammarObject } from './GrammarObject';
 
-export class Determiner extends GrammarObject { }
+export class Determiner extends GrammarObject {
+  // private static readonly A = 'a'; // TODO as a(n)
+
+  private static readonly MY = 'my';
+
+  private static readonly ONE = 'one';
+
+  private static readonly SOME = 'some';
+
+  private static readonly SUFFICIENT = 'sufficient';
+
+  private static readonly THAT = 'that';
+
+  private static readonly THE = 'the';
+
+  private static readonly THIS = 'this';
+
+  private static readonly YOUR = 'your';
+
+  public constructor() {
+    const options = [
+      // Determiner.A, // TODO as a(n)
+      Determiner.MY,
+      Determiner.ONE,
+      Determiner.SOME,
+      Determiner.SUFFICIENT,
+      Determiner.THAT,
+      Determiner.THE,
+      Determiner.THIS,
+      Determiner.YOUR,
+    ];
+
+    const randomInt = Math.floor(Math.random() * options.length); // TODO via Repository
+
+    super(options[randomInt]);
+  }
+}
--- src/Services/Words/src/Domain/Service/GrammarStream.ts
+++ src/Services/Words/src/Domain/Service/GrammarStream.ts
@@ -1,7 +1,9 @@
-import { GrammarObject } from '../Model/GrammarObject';
 import { AdjectiveRepository } from '../Repository/AdjectiveRepository';
+import { Determiner } from '../Model/Determiner';
+import { GrammarObject } from '../Model/GrammarObject';
 import { NounRepository } from '../Repository/NounRepository';
 import { PhraseFSM } from '../Model/PhraseFSM';
+
 
 export class GrammarStream {
   private stream: GrammarObject[];
@@ -14,7 +16,7 @@ export class GrammarStream {
     do {
       const { currentState } = fsm.next();
       if (currentState === PhraseFSM.STATE_1_DETERMINER) {
-        // retrieve Determiner
+        this.stream.push(new Determiner());
       } else if (currentState === PhraseFSM.STATE_2_ADJECTIVE) {
         this.stream.push(AdjectiveRepository.getOne());
       } else if (currentState === PhraseFSM.STATE_3_NOUN) {
